e36022dd0e4b905f93e498ca1c0f4ba811ab8512
PavelDubrov <dub1rov@yandex.ru>
2020-12-13 18:15:14 +0300

    for pull-req3

--- task_3.sql
+++
@@ -1,7 +0,0 @@
--- task_3
-SELECT vacancy_name, city_name, company_name FROM vacancy
-INNER JOIN city ON  city.city_id = vacancy.city_id
-INNER JOIN company ON company.company_id = vacancy.company_id
-WHERE compensation_from IS null AND compensation_to IS null
-ORDER BY created_at DESC
-LIMIT 10;
--- task_6.sql
+++
@@ -1,6 +0,0 @@
--- task_6
-select percentile_disc(0.5) within group (order by tmp.num_of_vac) as median
-from 
-(select company_id, count(vacancy_id) num_of_vac
-from vacancy
-group by company_id) as tmp;
