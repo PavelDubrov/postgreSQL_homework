98b84f718a48ec71d8e795b55bb3430c1f8aa7c8
PavelDubrov <dub1rov@yandex.ru>
2020-12-27 13:37:51 +0300

    for-pull-req7-fixed

--- task_5.sql
+++ task_5.sql
@@ -1,21 +1,18 @@
 -- task_5
-SELECT company_name, max_person
-FROM
-(
+WITH person_per_vac AS(
+	SELECT company_id, vacancy_name, COUNT(person_id) person_on_vac
+	FROM job_application
+	INNER JOIN vacancy ON vacancy.vacancy_id = job_application.vacancy_id
+	GROUP BY company_id, vacancy.vacancy_id
+	ORDER BY company_id
+), person_per_vac2 AS(
 	SELECT company_id, MAX(person_on_vac) AS max_person
-	FROM
-	(
-		SELECT company_id, vacancy_name, COUNT(person_id) person_on_vac
-		FROM job_application
-		INNER JOIN vacancy ON vacancy.vacancy_id = job_application.vacancy_id
-		GROUP BY company_id, vacancy_name
-		ORDER BY company_id
-	) AS tmp
-
+	FROM person_per_vac
 	GROUP BY company_id
 	ORDER BY max_person DESC
-) AS tmp2
-
-RIGHT JOIN company ON company.company_id = tmp2.company_id
+)
+SELECT company_name, max_person
+FROM person_per_vac2
+RIGHT JOIN company ON company.company_id = person_per_vac2.company_id
 ORDER BY max_person DESC NULLS LAST, company_name ASC
 LIMIT 5;
